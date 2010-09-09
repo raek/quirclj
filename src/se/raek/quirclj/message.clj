3213981efbb47703e8ccf26927dece86bd21568a
Rasmus Svensson <raek@lysator.liu.se>
2010-09-09 16:08:26 +0200

    For messages having sources that includes user name and host name, those are now put into the :source-user and :source-host keys. :source will now only contain the nickname.
    Nicknames are the only "entity identifiers" that can have optional parts (user name and host name). The optional parts must also be omitted when a nickname is used as a target. I therefore analyze the bare nicknames to be the "real" identifiers and the optional parts to be some kind of metadata.
    
    Also, the names of the new keys start with "source-" to signal that they belong together with the source.

--- src/se/raek/quirclj/message.clj
+++ src/se/raek/quirclj/message.clj
@@ -1,18 +1,30 @@
 (ns se.raek.quirclj.message
+  "Translate IRC messages between string and map form."
   {:author "Rasmus Svensson (raek)"}
   (:use [clojure.contrib.def :only [defvar-]]))
 
-(declare parse-params)
+(declare parse-source parse-params)
 
 (defvar- message-regex
   #"^(?::([^ ]+) +)?([^ ]+)(?: +(.+))?$")
 
 (defn parse [s]
-  (when-let [[_ prefix command param-str] (re-find message-regex s)]
-    (let [params (parse-params param-str)]
-      {:source prefix
-       :command command
-       :params params})))
+  (when-let [[_ source-str command param-str] (re-find message-regex s)]
+    (let [source-map (parse-source source-str)
+          params (parse-params param-str)]
+      (assoc source-map
+        :command command
+        :params params))))
+
+(defvar- source-regex
+  #"^([^!@]*)(?:!([^@]*))?(?:@(.*))?$")
+
+(defn- parse-source [source-str]
+  (let [[_ source user host]
+        (and source-str (re-find source-regex source-str))]
+       {:source source
+        :source-user user
+        :source-host host}))
 
 (defvar- param-regex
   #"(?:(?<!:)[^ :][^ ]*|(?<=:).*)")
--- test/se/raek/quirclj/test_message.clj
+++ test/se/raek/quirclj/test_message.clj
@@ -6,64 +6,109 @@
 (deftest test-parse
   
   (testing "command only"
-    (let [{:keys [source command params]}
+    (let [{:keys [source source-user source-host command params]}
           (msg/parse "FOO")]
-      (is (= source nil))
+      (is (nil? source))
+      (is (nil? source-user))
+      (is (nil? source-host))
       (is (= command "FOO"))
-      (is (= params nil))))
+      (is (nil? params))))
   
   (testing "prefix and command"
-    (let [{:keys [source command params]}
+    (let [{:keys [source source-user source-host command params]}
           (msg/parse ":tortoise BAR")]
       (is (= source "tortoise"))
+      (is (nil? source-user))
+      (is (nil? source-host))
       (is (= command "BAR"))
-      (is (= params nil))))
+      (is (nil? params))))
   
   (testing "single parameter"
-    (let [{:keys [source command params]}
+    (let [{:keys [source source-user source-host command params]}
           (msg/parse "BAZ param")]
-      (is (= source nil))
+      (is (nil? source))
+      (is (nil? source-user))
+      (is (nil? source-host))
       (is (= command "BAZ"))
       (is (= params ["param"]))))
   
   (testing "prefix, command and single parameter"
-    (let [{:keys [source command params]}
+    (let [{:keys [source source-user source-host command params]}
           (msg/parse ":achilles QUUX param")]
       (is (= source "achilles"))
+      (is (nil? source-user))
+      (is (nil? source-host))
       (is (= command "QUUX"))
       (is (= params ["param"]))))
   
   (testing "three parameters"
-    (let [{:keys [source command params]}
+    (let [{:keys [source source-user source-host command params]}
           (msg/parse "COMMAND one two three")]
       (is (= source nil))
+      (is (nil? source-user))
+      (is (nil? source-host))
       (is (= command "COMMAND"))
       (is (= params ["one" "two" "three"]))))
   
   (testing "rest parameter"
-    (let [{:keys [source command params]}
+    (let [{:keys [source source-user source-host command params]}
           (msg/parse "COMMAND :rest")]
-      (is (= source nil))
+      (is (nil? source))
+      (is (nil? source-user))
+      (is (nil? source-host))
       (is (= command "COMMAND"))
       (is (= params ["rest"]))))
   
   (testing "empty rest parameter"
-    (let [{:keys [source command params]}
+    (let [{:keys [source source-user source-host command params]}
           (msg/parse "COMMAND :")]
-      (is (= source nil))
+      (is (nil? source))
+      (is (nil? source-user))
+      (is (nil? source-host))
       (is (= command "COMMAND"))
       (is (= params [""]))))
   
   (testing "rest parameter with spaces"
-    (let [{:keys [source command params]}
+    (let [{:keys [source source-user source-host command params]}
           (msg/parse "COMMAND :one two three")]
-      (is (= source nil))
+      (is (nil? source))
+      (is (nil? source-user))
+      (is (nil? source-host))
       (is (= command "COMMAND"))
       (is (= params ["one two three"]))))
   
   (testing "normal parameters and rest parameter"
-    (let [{:keys [source command params]}
+    (let [{:keys [source source-user source-host command params]}
           (msg/parse "COMMAND one two :three three three")]
-      (is (= source nil))
+      (is (nil? source))
+      (is (nil? source-user))
+      (is (nil? source-host))
       (is (= command "COMMAND"))
-      (is (= params ["one" "two" "three three three"])))))
+      (is (= params ["one" "two" "three three three"]))))
+
+  (testing "prefix with user name"
+    (let [{:keys [source source-user source-host command params]}
+          (msg/parse ":foo!bar COMMAND")]
+      (is (= source "foo"))
+      (is (= source-user "bar"))
+      (is (nil? source-host))
+      (is (= command "COMMAND"))
+      (is (= params nil))))
+
+  (testing "prefix with host name"
+    (let [{:keys [source source-user source-host command params]}
+          (msg/parse ":foo@example.com COMMAND")]
+      (is (= source "foo"))
+      (is (nil? source-user))
+      (is (= source-host "example.com"))
+      (is (= command "COMMAND"))
+      (is (= params nil))))
+
+  (testing "prefix with user name and host name"
+    (let [{:keys [source source-user source-host command params]}
+          (msg/parse ":foo!bar@example.com COMMAND")]
+      (is (= source "foo"))
+      (is (= source-user "bar"))
+      (is (= source-host "example.com"))
+      (is (= command "COMMAND"))
+      (is (= params nil)))))
