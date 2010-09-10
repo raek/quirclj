49974e8ba52f538d1f4e9adea8bb0e345f3e7b4d
Rasmus Svensson <raek@lysator.liu.se>
2010-09-10 15:07:25 +0200

    Added namespace documentation for se.raek.quirclj.message explaining the string and map forms of IRC messages.

--- src/se/raek/quirclj/message.clj
+++ src/se/raek/quirclj/message.clj
@@ -1,5 +1,34 @@
 (ns se.raek.quirclj.message
-  "Translate IRC messages between string and map form."
+  "Translate IRC messages between string and map form.
+
+   The syntax of IRC message strings is defined in RFC 1459. This namespace
+   introduces another representation for IRC messages using Clojure maps and
+   functions for converting between the two.
+
+   An IRC message has the following anatomy:
+
+   - an optional source (called \"prefix\" in the RFC),
+   - a command,
+   - and zero or more parameters, of which the last one can contain spaces.
+
+   The source can also have optional user name and host name information
+   attached to it.
+
+   The map form of a message has the following keys:
+
+   :source       source as a string (without user name and user host) or nil
+   :source-user  user name of the source as a string or nil
+   :source-host  host name of the source as a string or nil
+   :command      as-is command name as string
+   :params       parameters as a sequence of strings or nil if none
+
+   Examples:
+     (parse \":achilles!~achilles@example.com PRIVMSG #quirclj :Hello there!\")
+     => {:source \"achilles\"
+         :source-user \"~achilles\"
+         :source-host \"example.com\"
+         :command \"PRIVMSG\"
+         :params (\"#quiclj\" \"Hello there!\")}"
   {:author "Rasmus Svensson (raek)"}
   (:use [clojure.contrib.def :only [defvar-]]))
 
